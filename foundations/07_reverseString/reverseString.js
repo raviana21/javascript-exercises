4401ff31d879d7ba90fd6d7c24cf5ab49ebf4922
raviana21 <209082027+raviana21@users.noreply.github.com>
2026-06-30 15:22:40 +0800

    Create string reverse fn & test

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split('');
+    let arr = splitString.reverse();
+    return arr.join('')
 };
 
 // Do not edit below this line
